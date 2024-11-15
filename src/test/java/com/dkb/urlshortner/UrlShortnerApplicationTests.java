35a1a5408716a59a3d4492ee76ab6ae8768ba7ee
Sai Sanakkayala <rmsaikris7@gmail.com>
2024-11-15 21:10:10 +0100

    Initial commit

---
+++ src/test/java/com/dkb/urlshortner/UrlShortnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkb.urlshortner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
