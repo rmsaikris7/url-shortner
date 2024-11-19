aaae3936343199dd3542d539f473dc69ddf2dbc6
Sai Sanakkayala <rmsaikris7@gmail.com>
2024-11-19 13:24:17 +0100

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.5"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.dkb.urlshortner"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 java {
 	toolchain {
@@ -18,15 +19,43 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-jooq")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.flywaydb:flyway-core")
-	implementation("org.flywaydb:flyway-database-postgresql")
-	runtimeOnly("org.postgresql:postgresql")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// spring dependencies
+	implementation(libs.springBootStarterDataJpa)
+	implementation(libs.springBootStarterDataRedis)
+	implementation(libs.springBootStarterWeb)
+
+	// external dependencies
+	annotationProcessor(libs.mapstructProcessor)
+	implementation(libs.flywayCore)
+	implementation(libs.flywayDatabasePostgresql)
+	implementation(libs.jakartaValidationApi)
+	implementation(libs.commonsCodec)
+	implementation(libs.mapstruct)
+	implementation(libs.hibernateValidator)
+	implementation(libs.dbSchedulerSpringBootStarter)
+	implementation(libs.springdocWebmvcUI)
+	implementation(libs.spotlessLib)
+	implementation(libs.spotlessPluginGradle)
+	runtimeOnly(libs.postgresql)
+
+	// test dependencies
+	testImplementation(libs.springBootStarterTest)
+	testImplementation(libs.testcontainersJunitJupiter)
+	testImplementation(libs.testcontainersPostgresql)
+	testImplementation(libs.restAssured)
+	testRuntimeOnly(libs.junitPlatformLauncher)
+}
+
+spotless {
+	java {
+		target("src/*/java/**/*.java")
+		toggleOffOn()
+		palantirJavaFormat()
+		removeUnusedImports()
+		trimTrailingWhitespace()
+		endWithNewline()
+		formatAnnotations()
+	}
 }
 
 tasks.withType<Test> {
